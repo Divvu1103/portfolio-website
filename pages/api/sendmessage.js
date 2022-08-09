6dedc78660dc3aae27466f3a69e918fc03e9a197
Divvu1103 <200110075@hbtu.ac.in>
2022-08-09 14:39:32 +0530

    m

--- pages/api/sendmessage.js
+++ pages/api/sendmessage.js
@@ -2,41 +2,7 @@ import client from "../../lib/client";
 import nc from "next-connect";
 import axios from "axios";
 import { data } from "autoprefixer";
-// export default async function handler(
-//     req,
-//     res
-//   ) {
-//     const data = JSON.parse(req.body)
-  
-//     const mutations = {
-//       mutations: [
-//         {
-//           create: {
-//             _type: 'tweet',
-//             text: data.text,
-//             username: data.username,
-//             blockTweet: false,
-//             profileImg: data.profileImg,
-//             image: data.image,
-//           },
-//         },
-//       ],
-//     }
-  
-//     const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
-  
-//     const result = await fetch(apiEndpoint, {
-//       headers: {
-//         'content-type': 'application/json',
-//         Authorization: `Bearer ${process.env.SANITY_API_TOKEN}`,
-//       },
-//       body: JSON.stringify(mutations),
-//       method: 'POST',
-//     })
-//     const json = await result.json()
-  
-//     res.status(200).json({ message: 'Added!' })
-//   }
+
 export default async function handler( req, res)  {
   const projectId = '3yvwgusg';
   const dataset = 'production';
